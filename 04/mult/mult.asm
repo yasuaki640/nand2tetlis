5c0edcbdfe077bbcb5c19f1b995667a9c0e0bc64
yasuaki640 <yasuaki640@gmail.com>
2020-01-16 21:29:52 +0900

    第四章　機械語

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -5,5 +5,31 @@
 
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+// D is Data. M is Memori. A is Access
 
 // Put your code here.
+
+//R0*R1を求める
+
+    @R2 //R2へのアクセスメモリを取得？
+    M=0 //R2の値が格納されているメモリに0を代入する // R2 = 0
+    @i
+    M=1 // i = 1
+(LOOP)
+    @i
+    D=M // = i
+    @R1
+    D=D-M //D=i-R1
+    @END
+    D;JGT //if(i-R)>0, goto END
+    @R0
+    D=M //D=R[0]
+    @R2
+    M=D+M //R2 += R0
+    @i
+    M=M+1 // i++
+    @LOOP
+    0;JMP // Goto LOOP
+(END)
+    @END
+    0;JMP //Goto END(infinite loop)
