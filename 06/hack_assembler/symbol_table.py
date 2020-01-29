87a8ec6e3a227a1e1a6de3ceb808f916e546db55
yasuaki640 <yasuaki640@gmail.com>
2020-01-29 21:42:14 +0900

    Create symbol_table.py

---
+++ 06/hack_assembler/symbol_table.py
@@ -0,0 +1,46 @@
+# アセンブラのシンボルを各アドレスに解決するためのクラス
+
+class SymbolTable():
+    def __init__(self):
+        self.symbol_table = {
+            'SP': 0,
+            'LCL': 1,
+            'ARG': 2,
+            'THIS': 3,
+            'THAT': 4,
+            'R0': 0,
+            'R1': 1,
+            'R2': 2,
+            'R3': 3,
+            'R4': 4,
+            'R5': 5,
+            'R6': 6,
+            'R7': 7,
+            'R8': 8,
+            'R9': 9,
+            'R10': 10,
+            'R11': 11,
+            'R12': 12,
+            'R13': 13,
+            'R14': 14,
+            'R15': 15,
+            'SCREEN': 16384,
+            'KBD': 24576
+        }
+        self.next_address = 16
+        
+    # テーブルに(symbol,adress)のペアを追加する
+    def add_entry(self, symbol, address):
+        self.symbol_table[symbol] = address
+    
+    def add_variable(self, symbol):
+        self.add_entry(symbol, self.next_address)
+        self.next_address += 1
+        
+    # シンボルテーブルは与えられたシンボルを含むかチェック
+    def contains(self, symbol):
+        return self.symbol_table.has_key(symbol)
+
+    # symbolに結びつけられたアドレスを返す
+    def get_address(self, symbol):
+        return self.symbol_table[symbol]
